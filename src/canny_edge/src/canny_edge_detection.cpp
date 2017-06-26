21d9946db2ae8644a402cb9c5c7493d6ed516946
Salahuddin_Alcor <uddin@dis.uniroma1.it>
2017-06-26 21:27:29 +0200

    Edge Detection and publish detected image update

--- src/canny_edge/src/canny_edge_detection.cpp
+++ src/canny_edge/src/canny_edge_detection.cpp
@@ -25,22 +25,22 @@ static const char WINDOW[] = "Image window";
  
 class canny_edge_detection
 {
-  ros::NodeHandle nh_;
-  ros::NodeHandle n;
- ros::Publisher pub ;
-  image_transport::ImageTransport it_;    
-  image_transport::Subscriber image_sub_; //image subscriber 
-  image_transport::Publisher image_pub_; //image publisher(we subscribe to ardrone image_raw)
-  image_transport::Publisher image_pub_2;  
-  std_msgs::String msg;
+	ros::NodeHandle nh_;
+	ros::NodeHandle n;
+ 	ros::Publisher pub ;
+  	image_transport::ImageTransport it_;    
+  	image_transport::Subscriber image_sub_; //image subscriber 
+  	image_transport::Publisher image_pub_; //image publisher(we subscribe to ardrone image_raw)
+  	image_transport::Publisher image_pub_edge_;  
+  	std_msgs::String msg;
 public:
- canny_edge_detection()
-    : it_(nh_)
+ 	canny_edge_detection()
+    	: it_(nh_)
   {
  
-     image_sub_ = it_.subscribe("/cv_camera_node/image_raw", 1, &canny_edge_detection::imageCb, this);
-     image_pub_= it_.advertise("/camera/image_processed",1);
-     image_pub_2= it_.advertise("/opencv/canny_edge",1);
+     	image_sub_ = it_.subscribe("/cv_camera_node/image_raw", 1, &canny_edge_detection::imageCb, this);
+     	image_pub_= it_.advertise("/camera/image_processed",1);
+     	image_pub_edge_= it_.advertise("/detection/canny_edge",1);
  
  
   }
@@ -49,7 +49,8 @@ public:
   {
     cv::destroyWindow(WINDOW);
   }
- 
+
+
   void imageCb(const sensor_msgs::ImageConstPtr& original_image)
   {
     cv_bridge::CvImagePtr cv_ptr;
@@ -62,8 +63,7 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
- 
-/*
+
       Mat out1;
       Mat gray_out;
       Mat canny_out;
@@ -74,37 +74,32 @@ public:
       cv::GaussianBlur(gray_out, gray_out, Size(3, 3), 0, 0);
       cv::Canny(gray_out, canny_out, 50, 125, 3);
       cv::cvtColor(canny_out, gray_out1, CV_GRAY2BGR);
+
       cv::imshow( "CAMERA FEED", cv_ptr->image);
-      cv::imshow( "GRAY CAMERA", gray_out);
-      cv::imshow( "CANNY CAMERA", canny_out);
-      cv::imshow( "CANNY EDGE DETECTION",gray_out1);
+      // cv::imshow( "GRAY CAMERA", gray_out);
+      // cv::imshow( "CANNY CAMERA", canny_out);
+       cv::imshow( "CANNY EDGE DETECTION",gray_out1);
       cvWaitKey(2);
- */
-
-
-    // Apply Edge detector still not working in progress
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-	cv::circle(cv_ptr->image, cv::Point(50, 50), 100, CV_RGB(255,255,255));
-   
-    // Display Image on GUI Window
-    cv::imshow("Canny Edge Detection", cv_ptr->image);
-    cv::waitKey(3);
+ 
+ 	/// Create a Trackbar for user to enter threshold
+  	//createTrackbar( "Min Threshold:", "Canny Edge Detection", &lowThreshold, max_lowThreshold, CannyThreshold );
      
-      // publish the image
+      // publish the raw image 
 	image_pub_.publish(cv_ptr->toImageMsg());
-	image_pub_2.publish(cv_ptr->toImageMsg());
-      // image_pub_.publish(gray_out1->toImageMsg());
+
+      // Publish the image from after edge detection
+  	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", gray_out1).toImageMsg();
+	image_pub_edge_.publish(msg);
  
 }
 };
  
  
  
- 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "canny_edge_detection");
-  canny_edge_detection ic;
+  canny_edge_detection edge;
   ros::spin();
  
   return 0;
